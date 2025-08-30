c85899d036daa829d37a0063607528ed07a5b91e
Jason Gardner <im@jasongardner.co>
2025-08-30 09:42:06 -0500

    feat: add default values to zod schemas and update package metadata

--- lib/zodObjects.ts
+++ lib/zodObjects.ts
@@ -4,29 +4,43 @@ export const cubeSchema = z.object({
   name: z.string(),
   origin: z
     .array(z.number()).length(3)
-    .describe("Pivot point of the cube."),
+    .describe("Pivot point of the cube.")
+    .optional()
+    .default([0, 0, 0]),
   from: z
     .array(z.number()).length(3)
-    .describe("Starting point of the cube."),
+    .describe("Starting point of the cube.")
+    .optional()
+    .default([0, 0, 0]),
   to: z
     .array(z.number()).length(3)
-    .describe("Ending point of the cube."),
+    .describe("Ending point of the cube.")
+    .optional()
+    .default([1, 1, 1]),
   rotation: z
     .array(z.number()).length(3)
-    .describe("Rotation of the cube."),
+    .describe("Rotation of the cube.")
+    .optional()
+    .default([0, 0, 0]),
 });
 
 export const meshSchema = z.object({
   name: z.string(),
   position: z
     .array(z.number()).length(3)
-    .describe("Position of the mesh."),
+    .describe("Position of the mesh.")
+    .optional()
+    .default([0, 0, 0]),
   rotation: z
     .array(z.number()).length(3)
-    .describe("Rotation of the mesh."),
+    .describe("Rotation of the mesh.")
+    .optional()
+    .default([0, 0, 0]),
   scale: z
     .array(z.number()).length(3)
-    .describe("Scale of the mesh."),
+    .describe("Scale of the mesh.")
+    .optional()
+    .default([1, 1, 1]),
   vertices: z
     .array(
       z
@@ -34,5 +48,7 @@ export const meshSchema = z.object({
         .length(3)
         .describe("Vertex coordinates in the mesh.")
     )
-    .describe("Vertices of the mesh."),
+    .describe("Vertices of the mesh.")
+    .optional()
+    .default([]),
 });
